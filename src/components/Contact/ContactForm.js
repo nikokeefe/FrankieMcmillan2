144820bc0864411ec975a38055c794edbb6790dd
Nik O'Keefe <nikwillok@gmail.com>
2019-11-17 10:29:47 +1300

    setup netify forms

--- src/components/Contact/ContactForm.js
+++ src/components/Contact/ContactForm.js
@@ -3,52 +3,60 @@ import Title from '../Title';
 import styles from '../../css/contact.module.css';
 
 const ContactForm = () => {
-  return (
-    <section>
-      <Title title="contact" subtitle="me" />
-      <div className={styles.center}>
-        <form
-          action="https://formspree.io/nikokeefe3@gmail.com"
-          method="POST"
-          className={styles.form}
-        >
-          <div>
-            <label htmlFor="name">Name</label>
-            <input
-              type="input"
-              name="name"
-              id="name"
-              className={styles.formControl}
-              placeholder="Larry Crump"
-            />
-          </div>
-          <div>
-            <label htmlFor="email">Email</label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              className={styles.formControl}
-              placeholder="larry@email.com"
-            />
-          </div>
-          <div>
-            <label htmlFor="message">Message</label>
-            <textarea
-              name="message"
-              id="message"
-              rows="10"
-              className={styles.formControl}
-              placeholder="tell me a little something..."
-            />
-          </div>
-          <div>
-            <input type="submit" value="submit" className={styles.submit} />
-          </div>
-        </form>
-      </div>
-    </section>
-  );
+	return (
+		<section>
+			<Title title='contact' subtitle='me' />
+			<div className={styles.center}>
+				<div className={styles.section}>
+					<form
+						method='post'
+						name='contact'
+						action='/thanks'
+						data-netlify='true'
+						netlify-honeypot='bot'
+						className={styles.form}
+					>
+						<input type='hidden' name='form-name' value='contact' />
+						<div className={styles.hidden}>
+							<label>Don't fill this out, human</label>
+							<input name='bot' />
+						</div>
+
+						<div className={styles.field}>
+							<label htmlFor='name'>Name</label>
+							<input
+								type='input'
+								name='name'
+								id='name'
+								className={styles.formControl}
+							/>
+						</div>
+						<div className={styles.field}>
+							<label htmlFor='email'>Email</label>
+							<input
+								type='email'
+								name='email'
+								id='email'
+								className={styles.formControl}
+							/>
+						</div>
+						<div className={styles.field}>
+							<label htmlFor='message'>Message</label>
+							<textarea
+								name='message'
+								id='message'
+								rows='10'
+								className={styles.formControl}
+							/>
+						</div>
+						<div className={styles.field}>
+							<input type='submit' value='submit' className={styles.submit} />
+						</div>
+					</form>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default ContactForm;
---
+++ src/pages/thanks.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import AniLink from 'gatsby-plugin-transition-link/AniLink';
+
+import Layout from '../components/Layout';
+import StyledHero from '../components/StyledHero';
+
+import styles from '../css/contact.module.css';
+
+const thanks = ({ data }) => {
+	return (
+		<Layout>
+			<div className=''>
+				<StyledHero img={data.defaultBcg.childImageSharp.fluid} />
+				<div className={styles.thanks}>
+					<h1>Thank you! I'll be in touch soon.</h1>
+				</div>
+				<div className={styles.center}>
+					<AniLink cover className='btn-primary' to={`/`}>
+						back home
+					</AniLink>
+				</div>
+			</div>
+		</Layout>
+	);
+};
+
+export const query = graphql`
+	query {
+		defaultBcg: file(relativePath: { eq: "book_excerpt.jpg" }) {
+			childImageSharp {
+				fluid(quality: 90, maxWidth: 4160) {
+					...GatsbyImageSharpFluid_withWebp
+				}
+			}
+		}
+	}
+`;
+
+export default thanks;
